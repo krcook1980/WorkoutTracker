73a9e1d3f30d660478def618b3469373616560c7
Kelly Cook <kellyrcook1980@gmail.com>
2021-04-11 11:12:28 -0600

    html routes now working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,19 +22,37 @@ router.post("/api/workouts/:id", (req, res) => {
         },
         {
             $set: {
-
+//I don't know what goes in here... exercises: { body?}
             }
         }
     )
 })
 
 //new workout plan
-
+router.post("api/workouts", ({data}, res) => {
+    db.Workout.create(data)
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
 
 //add exercises to new workout plan
 
 //stats view weight of multiple exercises from past 7
-
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find()
+    .sort({_id: -1})
+    .limit(7)
+    .then(weightRange => {
+        res.json(weightRange)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+      });
+})
 //stats view total duration of each workout from past 7
 
 module.exports = router;
